9bfd69514412b9ab7e245e48fbb0884759e37d7b
Oliver Lomax <oliver.lomax@abingdon.org.uk>
2024-07-10 13:26:35 +0100

    first commit

---
+++ camera_test.py
@@ -0,0 +1,70 @@
+import cv2, numpy as np
+from datetime import datetime
+
+cv2.namedWindow('Original Image', cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('Ascii', cv2.WINDOW_NORMAL)
+
+def text_image(text: str, width: int, height: int):
+    char_size = 10
+    font_scaling = 0.8
+    colour = (100, 255, 0)
+    colour = (255, 255, 255)
+    font = cv2.FONT_HERSHEY_PLAIN
+    text_queue = text[:]
+    image = np.zeros((height*char_size,width*char_size,3), np.uint8)
+    text_x, text_y = 0, 10
+    while text_queue:
+        next_c, text_queue = text_queue[0], text_queue[1:]
+        
+        if next_c == '\n':
+            text_y += char_size
+            text_x = 0
+        else:
+            text_x += char_size
+            image = cv2.putText(image, next_c, (text_x,text_y), font, font_scaling, colour, 1, cv2.LINE_AA)
+
+    return image
+
+def ascii_vid(ascii_width: int = 150, chars: str= '.:-=+*#%@'):
+    # cap = cv2.VideoCapture('/dev/video4')
+    cap = cv2.VideoCapture(0)
+
+    if cap.isOpened():
+        ret, frame = cap.read()
+        orig_shape = frame.shape
+        orig_height, orig_width = orig_shape[:2]
+    else:
+        return False
+
+    ascii_height = int((ascii_width / orig_width) * orig_height)
+    print(f'{ascii_width=}, {ascii_height=}')
+    print(f'{orig_width=}, {orig_height=}')
+
+    while ret:
+        ret, frame = cap.read()
+        resized = cv2.resize(frame, (ascii_width,ascii_height), interpolation = cv2.INTER_AREA)
+        gray_scale = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+        # cv2.imshow('Original Image', gray_scale)
+        text = ''
+        for row in gray_scale:
+            for p in row:
+                text += chars[int(p/256 * len(chars))]
+            text += '\n'
+        cv2.imshow('Ascii', text_image(text, ascii_width, ascii_height))
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+        if key == 32:
+            path = './output/'
+            filename = f'ascii_capture_{datetime.now().strftime("%d%m%y%H%M%S")}.txt'
+            with open(path + filename, 'w') as file:
+                file.write(text)
+                print(f'ASCII capture saved to: {filename}')
+
+    cap.release()
+
+
+chars = r'''$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,"^`'. '''[::-1]
+# chars = ".,:iox#"
+ascii_vid(200, chars)  
+cv2.destroyAllWindows()
---
+++ squares.py
@@ -0,0 +1,76 @@
+import cv2
+import numpy as np
+
+def get_rect(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.medianBlur(gray, 5)
+    sharpen_kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]])
+    sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
+
+    # Threshold and morph close
+    thresh = cv2.threshold(sharpen, 127, 255, cv2.THRESH_BINARY_INV)[1]
+    thresh2 = cv2.adaptiveThreshold(sharpen,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,3,10)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+    close2 = cv2.morphologyEx(thresh2, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+
+    # Find contours and filter using threshold area
+    cnts = cv2.findContours(close2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+    # print(image.shape)
+
+    min_area = 5000
+    max_area = 500000000
+    image_number = 0
+    x,y,w,h = 1,1,1,1
+    for c in cnts:
+        area = cv2.contourArea(c)
+        if area > min_area and area < max_area:
+            x,y,w,h = cv2.boundingRect(c)
+            ROI = image[y:y+h, x:x+w]
+            # cv2.imwrite('ROI_{}.png'.format(image_number), ROI)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (36,12,255), 2)
+            image_number += 1
+    # cv2.imshow('sharpen', sharpen)
+    # cv2.imshow('close', close)
+    # cv2.imshow('close2', close2)
+
+    # cv2.imshow('thresh', thresh)
+    # cv2.imshow('thresh2', thresh2)
+    return (x,y,w,h)
+
+# Load image, grayscale, median blur, sharpen image
+image = cv2.imread('goban_test1.jpg')
+image = cv2.resize(image, (600, 400), interpolation= cv2.INTER_LINEAR)
+
+# image = get_rect(image)
+
+# cv2.imshow('image', image)
+cv2.waitKey()
+cap = cv2.VideoCapture(1)
+if cap.isOpened():
+        ret, frame = cap.read()
+        orig_shape = frame.shape
+        orig_height, orig_width = orig_shape[:2]
+else:
+    print("Error")
+
+while ret:
+    ret, frame = cap.read()
+    resized = cv2.resize(frame, (600,400), interpolation = cv2.INTER_AREA)
+    
+    (x,y,w,h) = get_rect(resized)
+    sw = w//8
+    sh = h//8
+    if sh > 0 and sw > 0:
+        for sx in range(x, x+w-sw, sw):
+            for sy in range(y, y+h-sh, sh):
+                cv2.rectangle(resized, (sx, sy), (sx + sw, sy + sh), (36,255,12), 2)
+    cv2.imshow("Video", resized)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
